941599891977ac1c3fbad1a50c2bd82e7069c718
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-25 18:55:15 +0530

    changed adim interface

--- mysite/shortener/admin.py
+++ mysite/shortener/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import UrlRecord
+# change admin interface
+admin.site.index_title = "Shortener admin"
+admin.site.site_header = "Shortener admin"
+admin.site.site_title = "Shortener admin"
+admin.site.site_url = "/"
+
+
+class UrlRecordAdmin(admin.ModelAdmin):
+    # things to go on admin page
+    list_display = ["original_url", "short_url"]
+    # can filter by date_created
+    list_filter = ["date_created"]
+    # allow search
+    search_fields = ["original_url", "short_url"]
+
+
+admin.site.register(UrlRecord, UrlRecordAdmin)
