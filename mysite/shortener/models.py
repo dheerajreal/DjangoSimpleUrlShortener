816cf5d0859f26ee47e9ddd911dc5a089098c515
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-25 18:54:25 +0530

    created model

--- mysite/shortener/models.py
+++ mysite/shortener/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from string import ascii_letters
+from random import choices
 
-# Create your models here.
+
+def short_url_generate():
+    short = choices(ascii_letters, k=5)
+    short = "".join(short)
+    return short
+
+
+class UrlRecord(models.Model):
+    original_url = models.URLField( blank=False, null=False)
+    short_url = models.CharField(
+        primary_key=True, editable=False, max_length=5, default=short_url_generate)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.original_url
