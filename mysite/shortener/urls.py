d04e6842f5169771880d45a336837cdf6df2a178
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-25 18:54:58 +0530

    created url for resolve

--- mysite/shortener/urls.py
+++ mysite/shortener/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, resolve
 urlpatterns = [
+   
+    path('<str:urltag>/', resolve, name="resolve"),
     path('', index, name="index"),
+
 ]
