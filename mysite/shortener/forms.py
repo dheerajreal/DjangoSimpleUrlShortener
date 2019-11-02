e503016211b8664f19fd635d995cdbc6fb0f53b9
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-11-02 14:18:11 +0530

    changed appearances

--- mysite/shortener/forms.py
+++ mysite/shortener/forms.py
@@ -3,6 +3,7 @@ from .models import UrlRecord
 
 
 class UrlForm(forms.ModelForm):
+    original_url = forms.URLField(widget=forms.TextInput(attrs={"class": "form-control"})) 
     class Meta:
         model = UrlRecord
         fields = ["original_url"]
