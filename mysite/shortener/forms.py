8b13ee8523779056366eba56947d861ec1259660
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-11-08 21:59:41 +0530

    changed the form label and navbar style

--- mysite/shortener/forms.py
+++ mysite/shortener/forms.py
@@ -3,7 +3,9 @@ from .models import UrlRecord
 
 
 class UrlForm(forms.ModelForm):
-    original_url = forms.URLField(widget=forms.TextInput(attrs={"class": "form-control"})) 
+    original_url = forms.URLField(label="Original URL", widget=forms.TextInput(
+        attrs={"class": "form-control"}))
+
     class Meta:
         model = UrlRecord
         fields = ["original_url"]
