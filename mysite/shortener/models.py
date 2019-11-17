440f08afca1c34fd3f2a2a81ef54d27582e0844e
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-11-17 09:43:51 +0530

    added digits in shortcode

--- mysite/shortener/models.py
+++ mysite/shortener/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from string import ascii_letters
+from string import ascii_letters, digits
 from random import choices
 
 
 def short_url_generate():
-    short = choices(ascii_letters, k=4)
+    all_characters = ascii_letters + digits
+    short = choices(all_characters, k=4)
     short = "".join(short)
     return short
 
 
 class UrlRecord(models.Model):
-    original_url = models.URLField( blank=False, null=False)
+    original_url = models.URLField(blank=False, null=False)
     short_url = models.CharField(
         primary_key=True, editable=False, max_length=4, default=short_url_generate)
     date_created = models.DateTimeField(auto_now_add=True)
