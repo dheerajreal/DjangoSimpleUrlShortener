6d230fb7d8a768568a2ea64a18b604f96cb95a81
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-12-05 23:06:57 +0530

    ensure unique urltags

--- mysite/shortener/models.py
+++ mysite/shortener/models.py
@@ -5,7 +5,7 @@ from .utilities import short_url_generate
 class UrlRecord(models.Model):
     original_url = models.URLField(blank=False, null=False)
     short_url = models.CharField(
-        primary_key=True, editable=False, max_length=4, default=short_url_generate)
+        primary_key=True, editable=False, max_length=4)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- mysite/shortener/views.py
+++ mysite/shortener/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 from .forms import UrlForm
 from .models import UrlRecord
+from .utilities import short_url_generate
 # Create your views here.
+
+
+def create_urltag():
+    urltag = short_url_generate()
+    qs_exists = UrlRecord.objects.filter(short_url=urltag).exists()
+    if qs_exists:
+        return create_urltag()
+    return urltag
 
 
 def index(request):
@@ -14,15 +23,17 @@ def index(request):
             url_record = UrlRecord.objects.get(original_url=original_url)
         except:
             # create in database and fetch
+            form.instance.short_url = create_urltag()  # generate urltag
             form.save()
-            url_record = get_object_or_404(UrlRecord, original_url=original_url)
+            url_record = get_object_or_404(
+                UrlRecord, original_url=original_url)
 
         urltag = url_record.short_url
         context = {
             "urltag": urltag
         }
         return render(request, "shortener/detail.html", context)
-        
+
     context = {
         "form": form
     }
