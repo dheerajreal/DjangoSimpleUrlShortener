26e54260df263d62e7cdc30203aaf5dc62cbaecb
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-12-11 20:33:12 +0530

    added some comments

--- mysite/shortener/utilities.py
+++ mysite/shortener/utilities.py
@@ -1,6 +1,10 @@
 from string import ascii_letters, digits
 from random import choices
 
+# 4 characters=62**4=14,776,336 combinations
+# 5 characters=62**5=916,132,832 combinations
+# 6 characters=62**6=56,800,235,584 combinations
+# pick one of these
 URLTAG_LENGTH = 4
 
 
